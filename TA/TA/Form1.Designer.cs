008d75a3c1962c898933280efa50aa1ca284ae80
Huarzurita <miguelx_1@hotmail.com>
2023-04-01 13:05:39 -0500

    Version 1.0
    FUNCIONALIDADES:
    -Abrir y Guardar Imágenes (.png, .bmp)
    -Filtro de imagen a tono de grises
    -Grafica de histograma
    
    FALTA:
    -Ecualización: el algoritmo no funciona en imágenes a color (sospecha de tampoco en blanco y negro)
    -Expansión de histograma (pendiente de revisar algoritmo)

---
+++ TA/TA/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.histogramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fIltroToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.expansionHistogramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ecualizacionHistogramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.filtroToolStripMenuItem,
+            this.histogramaToolStripMenuItem,
+            this.fIltroToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1137, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirArchivoToolStripMenuItem,
+            this.guardarImagenToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirArchivoToolStripMenuItem
+            // 
+            this.abrirArchivoToolStripMenuItem.Name = "abrirArchivoToolStripMenuItem";
+            this.abrirArchivoToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.abrirArchivoToolStripMenuItem.Text = "Abrir Imagen";
+            this.abrirArchivoToolStripMenuItem.Click += new System.EventHandler(this.abrirArchivoToolStripMenuItem_Click);
+            // 
+            // guardarImagenToolStripMenuItem
+            // 
+            this.guardarImagenToolStripMenuItem.Name = "guardarImagenToolStripMenuItem";
+            this.guardarImagenToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.guardarImagenToolStripMenuItem.Text = "Guardar Imagen";
+            this.guardarImagenToolStripMenuItem.Click += new System.EventHandler(this.guardarImagenToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // filtroToolStripMenuItem
+            // 
+            this.filtroToolStripMenuItem.Name = "filtroToolStripMenuItem";
+            this.filtroToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.filtroToolStripMenuItem.Text = "TonosGris";
+            this.filtroToolStripMenuItem.Click += new System.EventHandler(this.filtroToolStripMenuItem_Click);
+            // 
+            // histogramaToolStripMenuItem
+            // 
+            this.histogramaToolStripMenuItem.Name = "histogramaToolStripMenuItem";
+            this.histogramaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.histogramaToolStripMenuItem.Text = "Histograma";
+            this.histogramaToolStripMenuItem.Click += new System.EventHandler(this.histogramaToolStripMenuItem_Click);
+            // 
+            // fIltroToolStripMenuItem1
+            // 
+            this.fIltroToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.expansionHistogramaToolStripMenuItem,
+            this.ecualizacionHistogramaToolStripMenuItem});
+            this.fIltroToolStripMenuItem1.Name = "fIltroToolStripMenuItem1";
+            this.fIltroToolStripMenuItem1.Size = new System.Drawing.Size(138, 20);
+            this.fIltroToolStripMenuItem1.Text = "ProcesamientoImagen";
+            // 
+            // expansionHistogramaToolStripMenuItem
+            // 
+            this.expansionHistogramaToolStripMenuItem.Name = "expansionHistogramaToolStripMenuItem";
+            this.expansionHistogramaToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.expansionHistogramaToolStripMenuItem.Text = "Expansion Histograma";
+            this.expansionHistogramaToolStripMenuItem.Click += new System.EventHandler(this.expansionHistogramaToolStripMenuItem_Click);
+            // 
+            // ecualizacionHistogramaToolStripMenuItem
+            // 
+            this.ecualizacionHistogramaToolStripMenuItem.Name = "ecualizacionHistogramaToolStripMenuItem";
+            this.ecualizacionHistogramaToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.ecualizacionHistogramaToolStripMenuItem.Text = "Ecualizacion Histograma";
+            this.ecualizacionHistogramaToolStripMenuItem.Click += new System.EventHandler(this.ecualizacionHistogramaToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 762);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TA_Grupo3";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem abrirArchivoToolStripMenuItem;
+        private ToolStripMenuItem guardarImagenToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem filtroToolStripMenuItem;
+        private ToolStripMenuItem histogramaToolStripMenuItem;
+        private ToolStripMenuItem fIltroToolStripMenuItem1;
+        private ToolStripMenuItem expansionHistogramaToolStripMenuItem;
+        private ToolStripMenuItem ecualizacionHistogramaToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ TA/TA/Form1.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TA
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap original;
+        private Bitmap resultante;
+        private int[] histograma = new int[256];
+
+        private int anchoVentana, altoVentana;
+        public Form1()
+        {
+            InitializeComponent();
+
+            resultante = new Bitmap(800, 600);
+
+            anchoVentana = 800;
+            altoVentana = 600;
+        }
+
+        
+        //Procesamiento de Imagen
+        private void expansionHistogramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int minValue = 255;
+            int maxValue = 0;
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    int pixelValue = original.GetPixel(i, j).R;
+                    if (pixelValue < minValue)
+                    {
+                        minValue = pixelValue;
+                    }
+                    if (pixelValue > maxValue)
+                    {
+                        maxValue = pixelValue;
+                    }
+                }
+            }
+
+            for (int i = 0; i < original.Width; i++)
+            {
+                for (int j = 0; j < original.Height; j++)
+                {
+                    int pixelValue = original.GetPixel(i, j).R;
+                    double expandedValue = ((double)(pixelValue - minValue) / (double)(maxValue - minValue)) * 255.0;
+                    resultante.SetPixel(i, j, Color.FromArgb((int)expandedValue, (int)expandedValue, (int)expandedValue));
+                }
+            }
+
+
+            this.Invalidate();
+        }
+
+        private void ecualizacionHistogramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 0; int y = 0;
+            int cOriginalR = 0;
+
+            int suma = 0;
+            int[] sumah = new int[256];
+
+            int tonoR = 0;
+
+            Color miColor;
+
+            double constante = 0;
+
+            for (x = 0; x < 255; x++)
+            {
+                suma = suma + histograma[x];
+                sumah[x] = suma;
+            }
+
+            constante = (double)(suma) / (double)(original.Width * original.Height);
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    cOriginalR = original.GetPixel(x, y).R;
+
+
+                    tonoR = (int)((double)sumah[cOriginalR] * constante);
+                    if (tonoR > 255) tonoR = 255;
+
+
+
+                    miColor = Color.FromArgb(tonoR, tonoR, tonoR);
+                    resultante.SetPixel(x, y, miColor);
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        //Hisograma
+        private void histogramaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 0; int y = 0;
+            Color rColor = new Color();
+
+            for (x = 0; x < original.Height; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    rColor = resultante.GetPixel(x, y);
+                    histograma[rColor.R]++;
+                    histograma[rColor.G]++;
+                    histograma[rColor.B]++;
+                }
+            }
+
+            Form2 hfrom = new Form2(histograma);
+            hfrom.Show();
+        }
+
+        //Filtro de Tono Gris
+        private void filtroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 0; int y = 0;
+            resultante = new Bitmap(original.Width, original.Height);
+            Color rColor = new Color();
+            Color oColor = new Color();
+
+            float g = 0;
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    oColor = original.GetPixel(x, y);
+
+                    g = oColor.R * 0.299f + oColor.G * 0.587f + oColor.B * 0.114f;
+
+                    rColor = Color.FromArgb((int)g, (int)g, (int)g);
+
+                    resultante.SetPixel(x, y, rColor);
+                }
+            }
+            this.Invalidate();
+        }
+
+        //Control de Archivos
+        private void abrirArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                original = (Bitmap)(Bitmap.FromFile(openFileDialog1.FileName));
+                anchoVentana = original.Width;
+                altoVentana = original.Height;
+
+                resultante = original;
+
+                this.Invalidate();
+            }
+        }
+        private void guardarImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                resultante.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (resultante != null)
+            {
+                Graphics g = e.Graphics;
+                AutoScrollMinSize = new Size(anchoVentana, altoVentana);
+                g.DrawImage(resultante,
+                    new Rectangle(this.AutoScrollPosition.X,
+                                    this.AutoScrollPosition.Y + 30,
+                                        anchoVentana, altoVentana));
+                g.Dispose();
+            }
+        }
+
+    }
+}
